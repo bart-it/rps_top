dc361a921fef40ad2bf8f6afcce282de6eef68d2
bartjo666 <bartjo666@gmail.com>
2019-08-26 20:20:38 +0100

    add check for incorrect user input and retry

--- index.js
+++ index.js
@@ -7,20 +7,26 @@ const playerPlay = txt => prompt(txt).toLowerCase();
 const computerPlay = () => rps[rand(rps.length)];
 
 const playRound = (computer, player) => {
-   if(computer === player) {
-       txt = 'Tie, choose again (Rock, Paper, Scissors?).'
-       return playRound(computerPlay(), playerPlay(txt));
-   } else if (
-       (computer + player === 'rockpaper')
-       ||(computer + player === 'paperscissors')
-       ||(computer + player === 'scissorsrock')
-   ){
-       playerScore++;
-       return `${player} beats ${computer}. You win!`
-   } else {
-       computerScore++;
-       return `${computer} beats ${player}. You lose!`;
+    if(rps.includes(player)){
+        if(computer === player) {
+            txt = 'Tie, choose again (Rock, Paper, Scissors?).'
+            return playRound(computerPlay(), playerPlay(txt));
+        } else if (
+            (computer + player === 'rockpaper')
+            ||(computer + player === 'paperscissors')
+            ||(computer + player === 'scissorsrock')
+        ){
+            playerScore++;
+            return `${player} beats ${computer}. You win!`
+        } else {
+            computerScore++;
+            return `${computer} beats ${player}. You lose!`;
+         }
+    } else {
+        txt = 'Not a correct format, choose between rock, paper or scissors.';
+        return playRound(computerPlay(), playerPlay(txt));
     }
+   
 };
 
 let txt = 'Rock, Paper, Scissors?'
